ea46aa358e167909c75598d70b545b0ac17aa1cf
Eunice Kim <euniceyunjkim@gmail.com>
2022-03-08 16:13:37 -0800

    Minimum requirements completed for pair sprint.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,58 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+import exampleVideoData from '../data/exampleVideoData.js';
+import searchYouTube from '../lib/searchYoutube.js';
+import Search from './Search.js';
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentVid: exampleVideoData[0],
+      videos: []
+    };
+
+    this.getYouTubeVideos('react tutorials');
+  }
+
+  getYouTubeVideos(search) {
+    searchYouTube(search, (videos) =>
+      this.setState({
+        currentVid: videos[0],
+        videos: videos,
+      })
+    );
+  }
+
+  onListItemClick(video) {
+    this.setState({
+      currentVid: video
+    });
+  }
+
+  render() {
+
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search handleSearchInputChange= {this.getYouTubeVideos.bind(this)}/>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVid} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} clickFunc={this.onListItemClick.bind(this)}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
